a02a7b35fb83639791d81547d498efa1df21ba56
廖维 <liaowei-0627@163.com>
2018-09-22 20:46:55 +0800

    重建版本库

--- code/flyhaze-study/study-service/src/main/java/com/flyhaze/study/service/ISysUserService.java
+++
@@ -1,8 +0,0 @@
-package com.flyhaze.study.service;
-
-import com.flyhaze.framework.service.IService;
-import com.flyhaze.study.entity.SysUser;
-import com.flyhaze.study.vo.UserVo;
-
-public interface ISysUserService extends IService<SysUser, UserVo> {
-}
